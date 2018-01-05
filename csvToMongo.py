8c22733eca224d28917434f5abc5c0c3cb88545e
adnanmc <adnanmcj@gmail.com>
2018-01-05 12:05:27 -0500

    initial code

---
+++ csvToMongo.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import csv
+import json
+import traceback
+import sys, os
+from collections import OrderedDict
+from pymongo import MongoClient
+
+
+def csv2mongo(csvfile, database_name,collection_name,delete_collection_before_import, host, port):
+    response_dict = OrderedDict()
+    try:
+        mc = MongoClient(host=host, port=port)
+        db = mc[database_name]
+        collection = db[collection_name]
+
+        if delete_collection_before_import == True:
+            db.drop_collection(collection)
+        # open the csv file.
+        csvhandle = csv.reader(open(csvfile, 'r'), delimiter=',')
+
+        rowindex = 0
+        mongoindex = 0
+        error_list = []
+
+        for row in csvhandle:
+
+            if rowindex == 0:
+                column_headers = row
+                cleaned_headers = []
+                for c in column_headers:
+                    c = c.replace(".", "")
+                    c = c.replace("(", "")
+                    c = c.replace(")", "")
+                    c = c.replace("$", "-")
+                    c = c.replace(" ", "_")
+                    cleaned_headers.append(c)
+            else:
+
+                record = OrderedDict(zip(cleaned_headers, row))
+                try:
+                    myobjectid = collection.insert(record)
+                    mongoindex += 1
+
+                except:
+                    error_message = "Error on row " + \
+                        str(rowindex) + ". " + str(sys.exc_info())
+                    error_list.append(error_message)
+
+            rowindex += 1
+
+        if error_list:
+            response_dict['num_rows_imported'] = rowindex
+            response_dict['num_rows_errors'] = len(error_list)
+            response_dict['errors'] = error_list
+            response_dict['code'] = 400
+            response_dict['message'] = "Completed with errors"
+        else:
+
+            response_dict['num_rows_imported'] = mongoindex
+            response_dict['num_csv_rows'] = rowindex
+            response_dict['code'] = 200
+            response_dict['message'] = "Completed."
+
+    except:
+        response_dict['code'] = 500
+        response_dict['errors'] = [traceback.print_exc()]
+
+    return response_dict
+
+
+# current directory
+currentDirectory = os.getcwd()
+csv_file = f'{currentDirectory}\\data\\data.csv'
+database = 'flights'
+collection = 'STG3Today'
+delete_collection_before_import = False
+host = '127.0.0.1'
+port = 27017
+
+result = csv2mongo(
+    csv_file,
+    database,
+    collection,
+    delete_collection_before_import,
+    host,
+    port)
+# output the JSON transaction summary
+print(json.dumps(result, indent=4))
